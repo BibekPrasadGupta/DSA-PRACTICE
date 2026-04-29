37698ff236250eb2b3c9cc93ced7af65b839eb39
Bibek Prasad Gupta <guptabibekprasad@gmail.com>
2026-04-29 21:21:12 +0530

    Add LeetCode 1480: Running Sum of 1D Array

--- LeetCode/Easy/LeetCode 1480: Running Sum of 1D Array.java
+++ LeetCode/Easy/LeetCode 1480: Running Sum of 1D Array.java
@@ -3,7 +3,7 @@
 **Platform:** LeetCode
 **Difficulty:** Easy
 **Language:** Java ☕
-
+    
 ---
 
 ## 📌 Problem Description
@@ -70,4 +70,4 @@ class Solution {
 
 ---
 
-*This solution is part of my Data Structures & Algorithms (DSA) practice in Java.*
+**This solution is part of my Data Structures & Algorithms (DSA) practice in Java.**
